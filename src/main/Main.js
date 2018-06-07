97afe82c35512a0f110f0fb91918c760d46b8f19
Farzan Ali <farzanali1296@gmail.com>
2018-06-06 23:43:03 -0400

    moved state and methods up to Main.js

--- src/main/Main.js
+++ src/main/Main.js
@@ -6,10 +6,64 @@ import Navigation from './navigation/Navigation';
 import Movies from './movies/Movies';
 
 class Main extends React.Component {
+
+    state = {
+        genre: 'Comedy',
+        genres: [],
+        // information for navigation sliders:
+        year: {
+            label: "year",
+            min: 1990,
+            max: 2018,
+            step: 1,
+            value: {min: 1996, max: 2018}
+        },
+        rating: {
+            label: "rating",
+            min: 0,
+            max: 10,
+            step: 1,
+            value: {min: 8, max: 10}
+        },
+        runtime: {
+            label: "runtime",
+            min: 0,
+            max: 300,
+            step: 15,
+            value: {min: 60, max: 120}
+        }
+    }
+    
+    //fetch genres from themoviedb and store them in the state:
+    componentDidMount() {
+        const genresURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`
+        fetch(genresURL)
+            .then(response => response.json())
+            .then(data => this.setState({ genres: data.genres }))
+            .catch(error => console.log(error))
+    }
+
+    onGenreChange = event => {
+        this.setState({ genre: event.target.value });
+    }
+
+    onChange = data => {
+        this.setState({
+            [data.type]: {
+                ...this.state[data.type], //previous value of e.g. this.state.year
+                value: data.value // overwrite the value property
+            }
+        });
+    };
+
     render() {
         return (
             <section className="main">
-                <Navigation />
+                <Navigation
+                    onChange={this.onChange}
+                    onGenreChange={this.onGenreChange}
+                    {...this.state}
+                />
                 <Movies />
             </section>
         )
--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -6,67 +6,20 @@ import Selection from './Selection';
 import Slider from './Slider';
 
 class Navigation extends React.Component {
-    state = {
-        genre: 'Comedy',
-        genres: [],
-        // information for navigation sliders:
-        year: {
-            label: "year",
-            min: 1990,
-            max: 2018,
-            step: 1,
-            value: {min: 1996, max: 2018}
-        },
-        rating: {
-            label: "rating",
-            min: 0,
-            max: 10,
-            step: 1,
-            value: {min: 8, max: 10}
-        },
-        runtime: {
-            label: "runtime",
-            min: 0,
-            max: 300,
-            step: 15,
-            value: {min: 60, max: 120}
-        }
-    }
     
-    //fetch genres from themoviedb and store them in the state:
-    componentDidMount() {
-        const genresURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`
-        fetch(genresURL)
-            .then(response => response.json())
-            .then(data => this.setState({ genres: data.genres }))
-            .catch(error => console.log(error))
-    }
-
-    onGenreChange = event => {
-        this.setState({ genre: event.target.value });
-    }
-
-    onChange = data => {
-        this.setState({
-            [data.type]: {
-                ...this.state[data.type], //previous value of e.g. this.state.year
-                value: data.value // overwrite the value property
-            }
-        });
-    };
-
     render() {
+        const { genre, genres, onGenreChange, onChange, year, rating, runtime } = this.props;
         return (
             <section className="navigation">
                 <Selection
-                    genre={this.state.genre}
-                    genres={this.state.genres}
-                    onGenreChange={this.onGenreChange}
+                    genre={genre}
+                    genres={genres}
+                    onGenreChange={onGenreChange}
                 />
 
-                <Slider data={this.state.year} onChange={this.onChange} />
-                <Slider data={this.state.rating} onChange={this.onChange} />
-                <Slider data={this.state.runtime} onChange={this.onChange} />
+                <Slider data={year} onChange={onChange} />
+                <Slider data={rating} onChange={onChange} />
+                <Slider data={runtime} onChange={onChange} />
 
             </section>
         )

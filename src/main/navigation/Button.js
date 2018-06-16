3e17ba38b07b0d64f66974646293e6736bd77b89
Farzan Ali <farzanali1296@gmail.com>
2018-06-16 12:47:02 -0400

    added comments to Button.js

--- src/main/navigation/Button.js
+++ src/main/navigation/Button.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import './Button.css';
 
+// The children property is passed in props. It is the content between the opening and closing tag of the component.
 const Button = ({ onClick, children }) => (
     <div className="search-button">
         <button onClick={onClick}>

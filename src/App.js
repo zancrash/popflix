37414f3336f56f9e2b55d5576a1d017025855279
Farzan Ali <farzanali1296@gmail.com>
2018-06-16 13:16:17 -0400

    wrapped app in BrowserRouter component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
-/*import logo from './logo.svg';*/
-/*import './App.css';*/
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Header from './header/Header';
 import Main from './main/Main';
+import Movies from './main/movies/Movies';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Main />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+          <Switch>
+            <Route exact path='/' component={Main} />
+            <Route path='/movies/:movieId' component={Movies} />
+          </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
 export default App;
